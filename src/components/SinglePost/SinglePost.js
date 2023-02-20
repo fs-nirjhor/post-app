6edc054de86031a800d2de2ea051eacb2a323267
fs-nirjhor <fsnirjhor2002@gmail.com>
2023-02-20 17:50:08 +0600

    commit

--- src/components/SinglePost/SinglePost.js
+++ src/components/SinglePost/SinglePost.js
@@ -1,12 +1,21 @@
 import * as React from 'react';
+import { useState } from "react";
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
+import { CardActionArea, CardActions } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import RecommendIcon from '@mui/icons-material/Recommend';
+import ChatIcon from '@mui/icons-material/Chat';
 import {Link} from 'react-router-dom' ;
 
 const SinglePost = (props) => {
   const {post} = props;
+  const [likeStyle, setLikeStyle] = useState("");
+  const handleLikeStyle = () => {
+    const newStyle = likeStyle ? "" : "primary";
+    setLikeStyle(newStyle);
+  };
 return (
   <div>
     <Card sx={{ margin: 2 }} key={post.id}>
@@ -20,14 +29,19 @@ return (
           </Typography>
         </CardContent>
       </CardActionArea>
-      { props.isButton &&
+      { 
+      props.isButton &&
       <CardActions>
-        <Button size="medium" color="primary" >
+        <IconButton aria-label="add to recommend">
+          <RecommendIcon onClick={handleLikeStyle} color={likeStyle} fontSize="large"/>
+        </IconButton>
       <Link to={`comments/${post.id}`}>
-          Comments
+          <IconButton aria-label="share">
+          <ChatIcon color="primary" fontSize="large"/>
+        </IconButton>
        </Link>
-        </Button>
-      </CardActions>}
+      </CardActions>
+      }
     </Card>
   </div>
 );
